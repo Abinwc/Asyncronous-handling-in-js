890f16de428bf38b9d6e4162b351822596775fcc
abinwc <abinpaul5598@gmail.com>
2021-01-09 00:41:06 +0530

    formatted promises.js

--- promises.js
+++ promises.js
@@ -1,30 +1,32 @@
 const blogs = [{ title: "Blog One", content: "This is blog one" },
-{ title: "Blog Two", content: "This is blog two" }];
+               { title: "Blog Two", content: "This is blog two" }];
 
 function createBlog(blog) {
-      return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             blogs.push(blog)
-             
-            const error= true;
 
-            if(!error){
+            const error = true;
+
+            if (!error) {
                 resolve();
             }
-            else{
+            else {
                 reject('Something went wrong');
             }
         }, 2000);
-      })
+    })
 }
 
 
 function getBlogs() {
     let output = '';
     blogs.forEach(blog => {
-        output += blog.title+"\n";
+        output += blog.title + "\n";
     });
     console.log(output)
 }
 
-createBlog({ title: "Blog Three", body: "This is Blog Three" }).then(getBlogs).catch(error=>console.log(error));
+createBlog({ title: "Blog Three", body: "This is Blog Three" })
+    .then(getBlogs)
+    .catch(error => console.log(error));
